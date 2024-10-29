d31eb871167c1362334656110a5452461a9a8850
Atanas Dimitrov <hi.atanasdim@gmail.com>
2024-10-29 20:42:05 +0000

    Update footer text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: FC = () => {
   return (
     <footer className="w-full flex justify-center items-center">
       <div className="max-w-2xl w-full px-16 pt-4 pb-8 text-xs text-center">
-        Created with NextJS, TalwindCSS & GSAP | 2024
+        Created with NextJS, TailwindCSS & GSAP | 2024
       </div>
     </footer>
   );
