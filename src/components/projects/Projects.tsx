8074597a39814fd06560186d817778c6669022b6
Atanas Dimitrov <hi.atanasdim@gmail.com>
2024-11-17 15:22:23 +0000

    Update projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -177,7 +177,7 @@ const Project: FC<ProjectDef> = ({
     containerRef,
     onScrollTriggerProgress,
     // Using width from useViewportSize because it doesn't triggers server side errors from window undefined
-    width < SM_BREAKPOINT ? 6 : 4,
+    width < SM_BREAKPOINT ? 6 : 2,
     width < SM_BREAKPOINT ? 2.6 : 1.2
   );
 
--- src/resources/projects.ts
+++ src/resources/projects.ts
@@ -1,8 +1,9 @@
-import { StaticImageData } from "next/image";
+import { StaticImageData } from 'next/image';
 
-import searchingMapboxImg from "@/assets/images/mapbox-search.jpg";
-import shadersPlaygroundImg from "@/assets/images/shaders-playground.jpg";
-import tiledImageLoaderImg from "@/assets/images/tiled-image-loader.jpg";
+import earthImg from '@/assets/images/3d-earth.jpg';
+import searchingMapboxImg from '@/assets/images/mapbox-search.jpg';
+import shadersPlaygroundImg from '@/assets/images/shaders-playground.jpg';
+import tiledImageLoaderImg from '@/assets/images/tiled-image-loader.jpg';
 
 export type ProjectDef = {
   themeColor: string;
@@ -19,62 +20,78 @@ function generateCloudinaryUrl(fileName: string, width: number) {
 }
 export const PROJECTS: ProjectDef[] = [
   {
-    themeColor: "#f1dec2",
+    themeColor: '#1f243b',
+    image: earthImg,
+    title: '3D Earth',
+    technologies: 'ThreeJS, TypeScript, NextJS',
+    links: [
+      {
+        label: 'Gist',
+        href: 'https://gist.github.com/atanas-dim/92177b10c2286ebfd865e1743c40354d',
+      },
+      {
+        label: 'Live',
+        href: 'https://earth-model-3d.vercel.app/',
+      },
+    ],
+  },
+  {
+    themeColor: '#f1dec2',
     videoSrc: generateCloudinaryUrl(
-      "tiled-image-loader-compressed_ba1if1.mp4",
+      'tiled-image-loader-compressed_ba1if1.mp4',
       400
     ),
     image: tiledImageLoaderImg,
-    title: "Tiled Image Loader",
-    technologies: "NextJS, TypeScript, TailwindCSS",
+    title: 'Tiled Image Loader',
+    technologies: 'NextJS, TypeScript, TailwindCSS',
     links: [
       {
-        label: "Code",
-        href: "https://github.com/atanas-dim/tiled-image-loader",
+        label: 'Code',
+        href: 'https://github.com/atanas-dim/tiled-image-loader',
       },
       {
-        label: "Live",
-        href: "https://tiled-image-loader.vercel.app/",
+        label: 'Live',
+        href: 'https://tiled-image-loader.vercel.app/',
       },
     ],
   },
   {
-    themeColor: "#f3e991",
+    themeColor: '#f3e991',
     videoSrc: generateCloudinaryUrl(
-      "shaders-playground-compressed_xqzupi.mp4",
+      'shaders-playground-compressed_xqzupi.mp4',
       400
     ),
     image: shadersPlaygroundImg,
-    title: "Shaders Playground",
-    technologies: "NextJS, TypeScript, ThreeJS, GLSL",
+    title: 'Shaders Playground',
+    technologies: 'NextJS, TypeScript, ThreeJS, GLSL',
     links: [
       {
-        label: "Code",
-        href: "https://github.com/atanas-dim/shaders-playground",
+        label: 'Code',
+        href: 'https://github.com/atanas-dim/shaders-playground',
       },
       {
-        label: "Live",
-        href: "https://shaders-playground-psi.vercel.app/",
+        label: 'Live',
+        href: 'https://shaders-playground-psi.vercel.app/',
       },
     ],
   },
   {
-    themeColor: "#3fa4ef",
+    themeColor: '#3fa4ef',
     videoSrc: generateCloudinaryUrl(
-      "searching-mapbox-compressed_ghrayz.mp4",
+      'searching-mapbox-compressed_ghrayz.mp4',
       400
     ),
     image: searchingMapboxImg,
-    title: "Searching Mapbox",
-    technologies: "React, TypeScript, Mapbox GL, SASS",
+    title: 'Searching Mapbox',
+    technologies: 'React, TypeScript, Mapbox GL, SASS',
     links: [
       {
-        label: "Code",
-        href: "https://github.com/atanas-dim/searching-mapbox",
+        label: 'Code',
+        href: 'https://github.com/atanas-dim/searching-mapbox',
       },
       {
-        label: "Live",
-        href: "https://searching-mapbox.netlify.app/",
+        label: 'Live',
+        href: 'https://searching-mapbox.netlify.app/',
       },
     ],
   },
